bef69fa8fe73b2ec7d5693f1e64b1bdab9fb79f1
wangtao <wangtao@iscs.com.cn>
2016-10-11 12:38:23 +0800

    angular material

---
+++ latest/js/build-config.js
@@ -0,0 +1,7 @@
+angular.module('docsApp').constant('BUILDCONFIG', {
+  "ngVersion": "1.5.5",
+  "version": "1.1.1",
+  "repository": "https://github.com/angular/material",
+  "commit": "180110e760b09af356b232d573bf67a644ae1910",
+  "date": "2016-09-02 10:29:08 -0700"
+});
